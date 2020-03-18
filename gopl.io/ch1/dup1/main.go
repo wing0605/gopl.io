10d075ab2cdb239871fbe64f422a0449a7ea5bdc
wing0605 <chentuliu@163.com>
2020-03-18 17:23:12 +0800

    dup1

--- gopl.io/ch1/dup1/main.go
+++ gopl.io/ch1/dup1/main.go
@@ -1,15 +1,25 @@
+// Dup1 prints the text of each line that appears more than
+// once in the standard input, preceded by its count.
+
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
 
 func main() {
-	var s, sep string
-	for i := 1; i < len(os.Args); i++ {
-		s += sep + os.Args[i]
-		sep = " "
+	//map存储了键/值（key/value）的集合，对集合元素，提供常数时间的存、取或测试操作。
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		counts[input.Text()]++
 	}
-	fmt.Println(s)
+	// NOTE: ignoring potential errors from input.Err()
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
 }
